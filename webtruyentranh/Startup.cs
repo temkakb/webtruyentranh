662977b0e9d186fa862829624378566758fe83c8
daingd <daingd214@gmail.com>
2021-06-22 19:06:01 +0700

    EF base

--- webtruyentranh/Startup.cs
+++ webtruyentranh/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -8,6 +9,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebTruyenTranhDataAccess.Context;
 
 namespace webtruyentranh
 {
@@ -24,6 +26,10 @@ namespace webtruyentranh
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<ComicContext>(opt =>
+           opt.UseSqlServer(Configuration.GetConnectionString("Default"))
+           .EnableSensitiveDataLogging()
+           .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
