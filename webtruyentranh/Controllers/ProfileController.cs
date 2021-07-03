e8106ba8642073feba8d13eab0695b76356b84f4
Linn <temkakb@gmail.com>
2021-07-03 13:56:18 +0700

    finish reply

--- webtruyentranh/Controllers/ProfileController.cs
+++ webtruyentranh/Controllers/ProfileController.cs
@@ -51,8 +51,9 @@ namespace webtruyentranh.Controllers
         [Authorize]
         public IActionResult Getprofile(long Id)
         {
-
+            Debug.WriteLine(Id);
             var profile = _db.Profiles.Include(p => p.Account).FirstOrDefault(p => p.Id == Id);
+            Debug.WriteLine(profile.DisplayName);
             var novels = _db.Novels.Where(n => n.Account.Id == profile.Account.Id).ToList();
             ViewBag.profile = profile;
             ViewBag.novels = novels;
@@ -62,18 +63,12 @@ namespace webtruyentranh.Controllers
             return View("Getprofile");
         }
 
-        [HttpGet]
-        public IActionResult Message()
-
-        {
-            return PartialView("_Messageparticalview");
-        }
         [HttpPost]
         [Authorize]
         public async Task<IActionResult> Message(String Content, long Id)
         {
             var SenderAccount = await userManager.GetUserAsync(User);
-            var ReciveAccount = userManager.Users.FirstOrDefault(a => a.Id == Id);
+            var ReciveAccount = userManager.Users.Include(a=>a.Profile).FirstOrDefault(a => a.Id == Id);
             Debug.WriteLine(Content);
             Debug.WriteLine(SenderAccount.UserName);
             Debug.WriteLine(ReciveAccount.UserName);
@@ -87,13 +82,41 @@ namespace webtruyentranh.Controllers
 
             });
             _db.SaveChanges();
-            return RedirectToAction("Getme", "Profile");
+            if (SenderAccount.Id == Id)
+            {
+                return RedirectToAction("Getme", "Profile");
+            }
+            else
+                return RedirectToAction("Getprofile", "Profile",new {Id= ReciveAccount.Profile.Id });
+        }
+        [Authorize]
+        [HttpGet]
+        public async Task< IActionResult> Reply(long Id)
+
+        { //get form reply for the mss
+            Debug.WriteLine(Id);
+            var account = await userManager.GetUserAsync(User);
+            var profile = _db.Profiles.Include(p => p.Account).Where(p => p.AccountId == account.Id).FirstOrDefault();
+            return ViewComponent("loadReply",new { profile = profile, Id = Id });
+        }
+        [Authorize]
+        [HttpPost]
+        public async Task<IActionResult> postreply (long Id, String Content, String ReturnUrl)
+
+        { //get form reply for the mss
+         
+            Message ms = _db.Messages.Where(ms => ms.Id == Id).FirstOrDefault();
+      ;
+          
+            var account = await userManager.GetUserAsync(User);
+            _db.ChildMessages.Attach(new ChildMessage { Account = account, Content = Content, CommentDate = DateTime.Now, Message = ms });
+            _db.SaveChanges();
+            return Redirect(ReturnUrl);
+
+
         }
 
-        public IActionResult Reply()
-        {
-            return null;
-        }
+
 
     }
 }
---
+++ webtruyentranh/Views/Shared/Components/loadReply/loadReply.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebTruyenTranhDataAccess.Context;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+using WebTruyenTranhDataAccess.Models;
+
+namespace webtruyentranh.Views.Shared.Components.loadReply
+{
+    public class loadReply : ViewComponent
+    {
+        public IViewComponentResult Invoke (Profile profile,long Id)
+          {
+            return View("_replyformpaticalview",profile);
+
+    }
+}
+}
