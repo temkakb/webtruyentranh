092887167f05243976a6b075462dd6c0a20ed8c2
Linn <temkakb@gmail.com>
2021-07-24 21:14:13 +0700

    fix episode

--- webtruyentranh/Controllers/EpisodeController.cs
+++ webtruyentranh/Controllers/EpisodeController.cs
@@ -11,7 +11,7 @@ using System.Threading.Tasks;
 using WebTruyenTranhDataAccess.Context;
 
 using WebTruyenTranhDataAccess.Models;
-
+using Microsoft.AspNetCore.Identity;
 namespace webtruyentranh.Controllers
 {
     public class EpisodeController : Controller
@@ -52,6 +52,7 @@ namespace webtruyentranh.Controllers
         
         [Route("novel/{novelId}/episode/Create")]
         [HttpGet]
+        [Authorize]
         public IActionResult Create(long novelId)
         {
             var model = new Episode();
@@ -61,6 +62,8 @@ namespace webtruyentranh.Controllers
 
         [Route("novel/{novelId}/episode/Create")]
         [HttpPost]
+        [ValidateAntiForgeryToken]
+    
         public IActionResult Create(Episode model)
         {
             
@@ -100,6 +103,8 @@ namespace webtruyentranh.Controllers
 
 
         [HttpGet]
+        [Authorize]
+
         public IActionResult Update(int id)
         {
             var ep = _context.Episodes.FirstOrDefault(m => m.Id == id);
@@ -115,11 +120,18 @@ namespace webtruyentranh.Controllers
         }
 
         [HttpPost]
-        public IActionResult Update(Episode model)
+        [ValidateAntiForgeryToken]
+      
+        public async Task< IActionResult> Update(Episode model)
         {
             if(ModelState.IsValid)
             {
-                var ep = _context.Episodes.FirstOrDefault(m => m.Id == model.Id);
+                var ep = _context.Episodes.Include(ep=>ep.Novel.Account).FirstOrDefault(m => m.Id == model.Id);
+                if(!ep.Novel.Account.UserName.Equals(User.Identity.Name))
+                {
+                    return PartialView("~/Views/Shared/_notfound.cshtml");
+
+                }
                 if(ep != null)
                 {
                     //ep.Id = model.Id;
@@ -138,6 +150,7 @@ namespace webtruyentranh.Controllers
         }
 
         [HttpPost]
+        [Authorize]
         public IActionResult Delete(int id)
         {
             var ep = _context.Episodes.FirstOrDefault(m => m.Id == id);
